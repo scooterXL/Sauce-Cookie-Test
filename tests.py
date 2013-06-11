3a469e71cdd231a2be0a8ab0b252b61d411d527c
Santiago Suarez Ordoñez <santiycr@gmail.com>
2013-06-11 16:25:08 -0400

    fixing bug by porting to Selenium 2

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
-from selenium import selenium
+from selenium.webdriver import Remote
 import unittest
+import os
 
 # HTTP/1.0 200 OK
 # Content-Type: text/html; charset=utf-8
@@ -9,62 +10,50 @@ import unittest
 # Set-Cookie: ccc2=stuff; Path=/
 # Server: Werkzeug/0.8.1 Python/2.7.2
 # Date: Fri, 18 Nov 2011 14:55:12 GMT
-# 
-# 
+#
+#
 #         <html>
 #             <head><title>Cookie Test</title></head>
 #             <body>Dropped cookies under name <em>ccc</em></body>
 #         </html>
-# 
+#
+
 
 class CookieTest(object):
 
     def test_cookies(self):
-        for N in range(1,4):
+        for N in range(1, 4):
             browser = self.browser
-            browser.delete_all_visible_cookies()
-            browser.open("/test/%s" % N)
+            browser.get("http://127.0.0.1:5000/test/%s" % N)
+            cookies = browser.get_cookies()
             for n in range(N):
-                # Note that when this fails it seems to be last in wins
-                self.assertTrue(browser.is_cookie_present('test%d' % n),
-                                "Missing cookie test%d" % n)
+                assert 'test%s' % n in [f['name'] for f in cookies], "Fail: test%s not in %s" % (n, cookies)
 
 
 class Sauce(unittest.TestCase, CookieTest):
 
     def setUp(self):
-        self.browser = selenium(
-            '127.0.0.1',
-            4445,
-            """{\
-                "username": "swidealist",\
-                "access-key": "d87a3a3d-4b8d-45a1-8f0b-c005926b55ed",\
-                "os": "Windows 2003",\
-                "browser": "firefox",\
-                "browser-version": "7",\
-                "name": "Multiple Cookie Bug Test." \
-               }""",
-               'http://127.0.0.1:5000')
-        self.browser.start()
-        self.browser.set_timeout(90000)
+        caps = {"platform": "Windows 2003",
+                "browserName": "firefox",
+                "version": "21",
+                "name": "Multiple Cookie Bug Test."}
+        self.browser = Remote('http://%(SAUCE_USER)s:%(SAUCE_KEY)s@127.0.0.1:4445/wd/hub' % os.environ,
+                              desired_capabilities=caps)
 
     def tearDown(self):
-        self.browser.stop()
+        self.browser.quit()
 
 
 class Localhost(unittest.TestCase, CookieTest):
 
     def setUp(self):
-        self.browser = selenium(
-            '127.0.0.1',
-            4444,
-            '*firefox',
-            'http://127.0.0.1:5000')
-        self.browser.start()
-        self.browser.set_timeout(90000)
+        caps = {"browserName": "chrome",
+                "name": "Multiple Cookie Bug Test."}
+        self.browser = Remote('http://127.0.0.1:4444/wd/hub',
+                                    desired_capabilities=caps)
 
     def tearDown(self):
-        self.browser.stop()
+        self.browser.quit()
 
 
 if __name__ == "__main__":
